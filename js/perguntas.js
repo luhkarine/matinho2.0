e0ce580e09901cc6240246f6d9f92385cd91191a
luhkarine <132585642+luhkarine@users.noreply.github.com>
2024-09-02 09:25:13 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -16,7 +16,7 @@ export const perguntas = [
                     "Por mais que a situação fosse ruim, fazer o bem seria muito arriscado para mim.",
                     "Eles estavam em grupo,e eu sozinho. A probabilidade de me baterem era bem alta."
                 ],
-                proxima: 2,
+                proxima: 1,
             },
         ]
     },
@@ -29,7 +29,7 @@ export const perguntas = [
                     "Diminuir a poluição é díficil,  porém você sabe que com pequenos esforços pode melhorar o meio ambiente.",
                     "Pode parecer que não vai fazer diferença, porém, se várias pessoas fizerem esse pequeno esforço, já se torna um grande esforço.",
                 ],
-                proxima: 3,
+                proxima: 2,
             },
             {
                 texto: Não há o que eu possa fazer, independente da boa ação, o mundo vai acabar um dia mesmo.",
@@ -37,72 +37,70 @@ export const perguntas = [
                     "Você ficou desacreditado de que o mundo possa melhorar.",
                     "Você é uma pessoa ruim, que só pensa em si mesma e não no futuro do planeta para o bem das outras gerações.",
                 ],
-                proxima: 4,
+                proxima: 2,
             },
         ]
     },
     {
-        enunciado: "Um chat com IA pode ser uma boa alternativa para realizar tarefas do dia a dia e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Durante a sala de aula, a professora apresenta um projeto voluntário sobre a conscientização o descarte correto do lixo e reciclagem, e convida os alunos a participarem, o que você decide fazer?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "É uma boa ideia, mesmo não valendo nota, eu deveria participar para tornar o mundo um lugar melhor.",
                 afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
-                ],
+                    "Separar o lixo é trabalhoso, entretando você sabe que é o melhor para o meio ambiente.",
+                    "você sabe que existe diversas coisas que podem ser feitas com lixo reclicável, como utilizar uma garrafa pet para fazer um vaso de planta ou fazer um puff com pneus carecas e tecido.",
+                 ],   
                 proxima: 3,
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto: "Eu não vou particiar, preciso fazer outras coisas além disso.",
                 afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                    "As outras preocupações da vida te impedem de enxergar o futuro do planeta.",
+                    "Você acha que é muito difícil separar o lixo ou ter ideias de como usar o lixo reciclável, porém você só tem a mente fechada e não encherga coisas boas nisso pelo seu preconceito com lixo ;).",
+                   ],
+                próxima:3
+            },
+        ]
+    },
+    {
+        enunciado: "Na escola, você esta andando com um amigo e ele joga um papel de bala no chão, você faz alguma coisa?",
+        alternativas: [
+            {
+                texto: "Eu corrigo ele e peço para ele pegar o papel do chão para descartar no ligar correto.",
+                afirmacao: [
+                    "Para você, o correto é 'lixo no lixo', se não tem um lixo proximo, segure-o até encontrar um local de descarte correto.",
+                    "você tem um coração bom e encherga a importância do descarte correto do lixo pois sabe do impacto ambientral que isso causa."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Eu não faço nada, um papel de bala não vai fazer diferença.",
+                afirmacao: [
+                    "Para você, um pequeno papel não faz diferença, mas para o planeta faz.",
+                    "você é imaturo e não liga para o impacto que esse tipo de atitude causa no meio ambiente, o quão prejudicial é para o seu futuro."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        enunciado: "A temperatura está razoavel,e o ar-condicionado esta ligado, você olha e pensa?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Deveria desliga-lo para economizar um pouco de energia e aproveitar o clima natural.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Pensando no própio consumo, você salvou um pouco do planeta para que as próximas gerações possam viver.",
                     "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Vou deixar ligado, se eu tenho vou usar mesmo.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Você pensa em si mesmo e não no impacto do meio ambiente, sendo assim, não aproveita o que a natureza pode te oferecer e impede as futuras gerações também.",
                     "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 6,
-            },
-        ]
-    },
-    {
-        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
                 proxima: 5,
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 6,
             },
         ]
     },
@@ -115,7 +113,7 @@ export const perguntas = [
                     "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
                     "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
-                proxima: 7,
+            
             },
             {
                 texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
@@ -123,49 +121,4 @@ export const perguntas = [
                     "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
                     "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: [
-                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
-                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
-                ],
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: [
-                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
-                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
-                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
-                ],
-            },
-        ]
-    }
-];
+             
